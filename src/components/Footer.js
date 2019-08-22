f96de4f1e8cf7a0ea60511c9ad28f79dba04faa8
Lorna Ryan <lornaniriain@gmail.com>
2019-08-21 21:16:56 -0400

    try to fix issue loading github page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react'
 class Footer extends React.Component {
     render() {
         return <footer className='fixed-bottom'>
-            <i class="fa fa-github"></i> 
             <a href="https://github.com/lornitar"> lornitar</a>
         </footer>
     }

4f207ea08b268d5f7ac6cb8824a2094a95af44af
Lorna Ryan <lornaniriain@gmail.com>
2019-09-11 22:14:33 -0400

    remove broken ravelry links insert images of projects and add styling

--- src/components/CrochetProject.js
+++ src/components/CrochetProject.js
@@ -7,7 +7,7 @@ class CrochetProject extends React.Component {
             <div className="crochetProject">
                 <h2>{this.props.project.name}</h2>
                 <p>{this.props.project.description}</p>
-                <a href={this.props.project.ravelry}>Take a look!</a>
+                <Image src={process.env.PUBLIC_URL + this.props.project.photo} alt={this.props.project.alt} thumbnail className="projectImage"></Image>
             </div>
         )
     }
